2222d89e49b831b361073dfc05a43ff0e0e1c2b3
Volodymyr Tsyhylyk <c_volodimir@ukr.net>
2022-04-10 03:29:54 +0300

    Final

---
+++ src/redux/operations.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getItems, postItem, deleteItem } from 'services/api';
+
+export const getContacts = createAsyncThunk(
+  'contacts/getContacts',
+  async (_state, { rejectWithValue }) => {
+    try {
+      return await getItems();
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
+export const postContact = createAsyncThunk(
+  'contacts/postContact',
+  async (contact, { rejectWithValue }) => {
+    try {
+      return await postItem(contact);
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, { rejectWithValue }) => {
+    try {
+      await deleteItem(id);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
